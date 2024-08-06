071af7a183d4ae182dc16119009bd566f6cdeced
NVCQN <nvcqnn@gmail.com>
2024-08-06 07:35:37 +0700

    comit

--- ctpro/components/QueryClientProvider.tsx
+++ ctpro/components/QueryClientProvider.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const ReactQueryClientProvider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(
@@ -16,5 +17,10 @@ export const ReactQueryClientProvider = ({ children }: { children: React.ReactNo
         },
       }),
   );
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  );
 };
---
+++ ctpro/provider/TanStackProvider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+//import { useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const TanStackProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient =  new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    {/* The rest of your application */}
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+  );
+};
+
+export default TanStackProvider;
