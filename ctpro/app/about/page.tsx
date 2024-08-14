ef0efa436450bb804c94849dd8ebbc795113d410
NVCQN <nvcqnn@gmail.com>
2024-08-14 23:10:20 +0700

    commit tối 14/08

--- ctpro/app/about/page.tsx
+++ ctpro/app/about/page.tsx
@@ -1,5 +1,62 @@
-// No React import needed up here! 😳
+'use client';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 export default function About() {
-  return <div>About</div>;
+  const pathname = usePathname();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button variant={'destructive'}>Bán ra</Button>
+      <Button variant={'secondary'}>Mua vào</Button>
+      <ul className="">
+        <li className="cursor-pointer border-b border-input px-1 py-2.5 font-semibold">
+          "1.hhlo"
+        </li>
+        <li className="px-1 pt-2.5">
+          {/* Remove Link from here and place it only on the links below */}
+          <Accordion
+            type="multiple"
+            className="w-full"
+            defaultValue={['accommodation']} // Set defaultValue to open the Accordion
+          >
+            <AccordionItem
+              className="border-stroke flex flex-col border-b"
+              value="accommodation"
+            >
+              <AccordionTrigger>
+                <div className="flex w-full items-center gap-2.5 text-sm font-semibold">
+                  My booking
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="flex w-full flex-col gap-2.5 text-sm font-semibold">
+                  <Link
+                    href="/about"
+                    className={`flex gap-2 px-5 py-2.5 ${pathname === '/about' ? 'text-blue-500' : ''}`}
+                  >
+                    HOTEL
+                  </Link>
+                  <Link
+                    href="/hotels/bookings/flight"
+                    className={`flex gap-2 px-5 py-2.5 ${pathname === '/hotels/bookings/flight' ? 'text-blue-500' : ''}`}
+                  >
+                    FLIGHT
+                  </Link>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </li>
+        <li className="cursor-pointer px-1 py-2.5 font-semibold">hello</li>
+      </ul>
+    </main>
+  );
 }
--- ctpro/services/rest/api-endpoint.ts
+++ ctpro/services/rest/api-endpoint.ts
@@ -2,4 +2,8 @@ export const API_ENDPOINTS = {
   GET_ALL_TIPY_POST: 'posts',
   GET_CAPTCHA: 'captcha',
   LOGIN_INVOICE: 'security-taxpayer/authenticate',
+  SEARCH_INVOICE_PURCHASE: 'query/invoices/purchase',
+  SEARCH_INVOICE_PURCHASE_SCO: 'sco-query/invoices/purchase',
+  SEARCH_INVOICE_SOLD: 'query/invoices/sold',
+  SEARCH_INVOICE_SOLD_SCO: 'sco-query/invoices/sold',
 };
